f2a5c121c7ee5edc7597db92dc15f51fd6f612c8
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-04-27 18:29:03 -0400

    Write Student class

--- src/main/java/com/example/projectapp/model/Student.java
+++ src/main/java/com/example/projectapp/model/Student.java
@@ -1,4 +1,73 @@
 package com.example.projectapp.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "students")
 public class Student {
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private Integer year;
+
+    @Column
+    private String house;
+
+    public Student() {
+    }
+
+    public Student(Long id, String name, Integer year, String house) {
+        this.id = id;
+        this.name = name;
+        this.year = year;
+        this.house = house;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getHouse() {
+        return house;
+    }
+
+    public void setHouse(String house) {
+        this.house = house;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", year=" + year +
+                ", house='" + house + '\'' +
+                '}';
+    }
 }
