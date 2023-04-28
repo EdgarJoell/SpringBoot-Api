45637c45658f2e644dace02caf6c0a4f66371ffe
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-04-28 12:18:02 -0400

    Make TeacherController a controller

--- src/main/java/com/example/projectapp/controller/TeacherController.java
+++ src/main/java/com/example/projectapp/controller/TeacherController.java
@@ -1,4 +1,12 @@
 package com.example.projectapp.controller;
 
+import com.example.projectapp.repository.TeacherRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/teachers/")
 public class TeacherController {
+
+    private TeacherRepository teacherRepository;
 }
