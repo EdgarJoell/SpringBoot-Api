42f108a6b91a64c4ca4c7ae9eb119755ef689e83
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-04-28 11:18:12 -0400

    Create fields for Class class

--- src/main/java/com/example/projectapp/model/Class.java
+++ src/main/java/com/example/projectapp/model/Class.java
@@ -1,9 +1,20 @@
 package com.example.projectapp.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "classes")
 public class Class {
+    @Id
+    @Column
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
 }
