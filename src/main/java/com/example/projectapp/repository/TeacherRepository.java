95b7fc1c7c0fcfa5114e6174f9742a401aae781e
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-04-28 17:39:46 -0400

    Make TeacherRepoitory extend from JpaRepository

--- src/main/java/com/example/projectapp/repository/TeacherRepository.java
+++ src/main/java/com/example/projectapp/repository/TeacherRepository.java
@@ -1,4 +1,7 @@
 package com.example.projectapp.repository;
 
-public interface TeacherRepository {
+import com.example.projectapp.model.Teacher;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeacherRepository extends JpaRepository<Teacher, Long> {
 }
