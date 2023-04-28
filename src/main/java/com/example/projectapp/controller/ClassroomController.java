c5834d8ee615a678f676349e3c9901c23667a6d0
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-04-28 17:35:05 -0400

    Write getClassrooms() method

--- src/main/java/com/example/projectapp/controller/ClassroomController.java
+++ src/main/java/com/example/projectapp/controller/ClassroomController.java
@@ -1,12 +1,15 @@
 package com.example.projectapp.controller;
 
+import com.example.projectapp.model.Classroom;
 import com.example.projectapp.service.ClassroomService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping(path = "/api/classes/")
+@RequestMapping(path = "/api/classrooms/")
 public class ClassroomController {
 
     public ClassroomService classroomService;
@@ -14,5 +17,10 @@ public class ClassroomController {
     @GetMapping(path = "/hello-world/")
     public String helloWorld() {
         return "Hello World! Or should I say...\n HELLO EDGAR";
+    }
+
+    @GetMapping(path = "/classrooms")
+    public List<Classroom> getClassrooms() {
+        return classroomService.getClassrooms();
     }
 }
--- src/main/java/com/example/projectapp/service/ClassroomService.java
+++ src/main/java/com/example/projectapp/service/ClassroomService.java
@@ -1,10 +1,17 @@
 package com.example.projectapp.service;
 
+import com.example.projectapp.model.Classroom;
 import com.example.projectapp.repository.ClassroomRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClassroomService {
 
     private ClassroomRepository classroomRepository;
+
+    public List<Classroom> getClassrooms () {
+        return classroomRepository.findAll();
+    }
 }
