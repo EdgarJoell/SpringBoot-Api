35e83fbff809843f6d64681bc1f9d407eea144df
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-04-28 12:35:42 -0400

    Connect StudentService with StudentRepository

--- src/main/java/com/example/projectapp/service/StudentService.java
+++ src/main/java/com/example/projectapp/service/StudentService.java
@@ -1,4 +1,10 @@
 package com.example.projectapp.service;
 
+import com.example.projectapp.repository.StudentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StudentService {
+
+    private StudentRepository studentRepository;
 }
