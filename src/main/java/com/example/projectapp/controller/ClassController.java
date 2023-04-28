57dff15437c73c81419758028d0314ae05e4c4af
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-04-28 11:42:48 -0400

    Test api with helloWorld() method (it's working)

--- src/main/java/com/example/projectapp/controller/ClassController.java
+++ src/main/java/com/example/projectapp/controller/ClassController.java
@@ -1,4 +1,18 @@
 package com.example.projectapp.controller;
 
+import com.example.projectapp.repository.ClassRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/classes/")
 public class ClassController {
+
+    public ClassRepository classRepository;
+
+    @GetMapping(path = "/hello-world/")
+    public String helloWorld() {
+        return "Hello World! Or should I say...\n HELLO EDGAR";
+    }
 }
