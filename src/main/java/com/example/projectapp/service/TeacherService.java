5073e7493513edf64ad22f7986e331d1c8a804b2
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-04-28 12:37:11 -0400

    Connect TeacherRepository with TeacherService

--- src/main/java/com/example/projectapp/service/TeacherService.java
+++ src/main/java/com/example/projectapp/service/TeacherService.java
@@ -1,4 +1,10 @@
 package com.example.projectapp.service;
 
+import com.example.projectapp.repository.TeacherRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TeacherService {
+
+    private TeacherRepository teacherRepository;
 }
