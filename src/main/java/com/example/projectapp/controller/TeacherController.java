a530f6a5cb747f058f401d210f1ea6f057597a5f
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-04-28 19:19:27 -0400

    Write getTeachers() method for Teacher

--- src/main/java/com/example/projectapp/controller/TeacherController.java
+++ src/main/java/com/example/projectapp/controller/TeacherController.java
@@ -1,12 +1,21 @@
 package com.example.projectapp.controller;
 
+import com.example.projectapp.model.Teacher;
 import com.example.projectapp.service.TeacherService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/teachers/")
 public class TeacherController {
 
     private TeacherService teacherService;
+
+    @GetMapping(path = "/teachers/")
+    public List<Teacher> getTeachers() {
+        return teacherService.getTeachers();
+    }
 }
--- src/main/java/com/example/projectapp/service/TeacherService.java
+++ src/main/java/com/example/projectapp/service/TeacherService.java
@@ -1,10 +1,17 @@
 package com.example.projectapp.service;
 
+import com.example.projectapp.model.Teacher;
 import com.example.projectapp.repository.TeacherRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TeacherService {
 
     private TeacherRepository teacherRepository;
+
+    public List<Teacher> getTeachers() {
+        return teacherRepository.findAll();
+    }
 }
