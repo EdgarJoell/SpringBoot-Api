592ee75f7388fbd6a14eed9024d5aa2fbc8be21e
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-04-28 19:04:23 -0400

    Write getStudents() method for Student

--- src/main/java/com/example/projectapp/controller/StudentController.java
+++ src/main/java/com/example/projectapp/controller/StudentController.java
@@ -1,12 +1,21 @@
 package com.example.projectapp.controller;
 
+import com.example.projectapp.model.Student;
 import com.example.projectapp.service.StudentService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api/students/")
 public class StudentController {
 
     private StudentService studentService;
+
+    @GetMapping(path = "/students/")
+    public List<Student> getStudents() {
+        return studentService.getStudents();
+    }
 }
--- src/main/java/com/example/projectapp/service/StudentService.java
+++ src/main/java/com/example/projectapp/service/StudentService.java
@@ -1,10 +1,17 @@
 package com.example.projectapp.service;
 
+import com.example.projectapp.model.Student;
 import com.example.projectapp.repository.StudentRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class StudentService {
 
     private StudentRepository studentRepository;
+
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
+    }
 }
