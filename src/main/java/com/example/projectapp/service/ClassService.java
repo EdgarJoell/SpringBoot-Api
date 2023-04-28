dda0b59c567b1e38ec5a886c3d6c8db6aad8b1a6
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-04-28 12:34:57 -0400

    Connect ClassService with ClassRepository

--- src/main/java/com/example/projectapp/service/ClassService.java
+++ src/main/java/com/example/projectapp/service/ClassService.java
@@ -1,4 +1,10 @@
 package com.example.projectapp.service;
 
+import com.example.projectapp.repository.ClassRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ClassService {
+
+    private ClassRepository classRepository;
 }
