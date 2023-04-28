4eb5fdd65239ebd1cf12aca91be9147d93ddb587
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-04-28 19:17:31 -0400

    Write method for findByName() for TeacherRepository

--- src/main/java/com/example/projectapp/repository/TeacherRepository.java
+++ src/main/java/com/example/projectapp/repository/TeacherRepository.java
@@ -4,4 +4,5 @@ import com.example.projectapp.model.Teacher;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TeacherRepository extends JpaRepository<Teacher, Long> {
+    Teacher findByName(String teacherName);
 }
