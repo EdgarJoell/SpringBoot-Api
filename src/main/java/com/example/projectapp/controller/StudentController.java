291fe29236b212df417552f8c20b86dd9468dbab
Edgar Zambrana <edgarzambrana@Edgars-MacBook-Pro.local>
2023-04-28 12:16:01 -0400

    Make StudentController a working controller

--- src/main/java/com/example/projectapp/controller/StudentController.java
+++ src/main/java/com/example/projectapp/controller/StudentController.java
@@ -1,4 +1,12 @@
 package com.example.projectapp.controller;
 
+import com.example.projectapp.repository.StudentRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/students/")
 public class StudentController {
+
+    private StudentRepository studentRepository;
 }
